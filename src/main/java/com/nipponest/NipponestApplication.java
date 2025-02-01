df3d4f16e3d7036a014b63aef38b6d9da86366a0
Rafael Correa <rafaelDev3590@gmail.com>
2025-01-31 21:33:33 -0300

    Criação da Base do projeto
    Criado a estrutura inicial e suas dependências, adicionei de cara o Spring Security pois quero tratar a parte de Autenticação antes de tudo.

---
+++ src/main/java/com/nipponest/NipponestApplication.java
@@ -0,0 +1,13 @@
+package com.nipponest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NipponestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NipponestApplication.class, args);
+	}
+
+}
