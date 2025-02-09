0a9e9f7a273c723f9e7a2df287b3f232fc4d18a7
Rafael Correa <rafaelDev3590@gmail.com>
2025-02-08 21:26:58 -0300

    feature/fluxo de autenticação

--- src/main/java/com/nipponest/config/WebConfig.java
+++ src/main/java/com/nipponest/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
     
